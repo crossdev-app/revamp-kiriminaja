363086effb67459ade39da1d62e575e45b1fd0a6
crossdev <malikdqa@gmail.com>
2022-12-24 02:05:28 +0700

    Init Project

---
+++ components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+    <nav>
+        <brandlogo class="nav-logo" />
+    </nav>
+</template>
+
+<script>
+import Brandlogo from './Brandlogo.vue'
+export default {
+  components: { Brandlogo },
+    name: "Navbar"
+}
+</script>
+
+<style>
+.nav-logo {
+    height: 100px;
+}
+</style>
